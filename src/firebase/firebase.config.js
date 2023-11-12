dd3bf4794710eac4bea2e0cc02bbd5e296fe247f
Rakib mia <rbepari404@gmail.com>
2023-11-12 02:19:08 -0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDsEfLl2rPfO7QLa0u1qZqM-RMugtqKebg",
+  authDomain: "stayvista-f9f25.firebaseapp.com",
+  projectId: "stayvista-f9f25",
+  storageBucket: "stayvista-f9f25.appspot.com",
+  messagingSenderId: "203313942274",
+  appId: "1:203313942274:web:da0be0b50cf95c51f0bc1b"
+};
+
+export const app = initializeApp(firebaseConfig);
